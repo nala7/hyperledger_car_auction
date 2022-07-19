684b3d1fe74cf24e8a2f7907552f727d4157e11a
Alejandro Labourdette <66191544+AlejandroLabourdette@users.noreply.github.com>
2022-07-19 21:31:07 +0200

    assetTransfer.go use auction.go functions

--- src/assetTransfer.go
+++ src/assetTransfer.go
@@ -61,6 +61,7 @@ func main() {
 
 	contract := network.GetContract("car-contract")
 
+	/****************************   Init Ledger   ***********************************/
 	log.Println("--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger")
 	result, err := contract.SubmitTransaction("InitLedger")
 	if err != nil {
@@ -68,50 +69,68 @@ func main() {
 	}
 	log.Println(string(result))
 
-	log.Println("--> Evaluate Transaction: GetCars, function returns all the current cars that are being auctioned")
-	result, err = contract.EvaluateTransaction("GetCars")
-	if err != nil {
-		log.Fatalf("Failed to evaluate transaction: %v", err)
-	}
-	log.Println(string(result))
-
-	log.Println("--> Evaluate Transaction: GetCar, return the car with the id specified")
-	result, err = contract.EvaluateTransaction("GetCar", "524")
+	/****************************   Utils   ***********************************/
+	log.Println("--> Evaluate Transaction: QueryCar, return the car with the id specified")
+	result, err = contract.EvaluateTransaction("QueryCar", "8")
 	if err != nil {
 		log.Fatalf("Failed to Evaluate transaction: %v", err)
 	}
 	log.Println(string(result))
 
-	log.Println("--> Submit Transaction: Create, function create an auction process for a car with details provided")
-	result, err = contract.SubmitTransaction("Create", "Lada", "Blue")
+	log.Println("--> Evaluate Transaction: QueryAllCars, function returns all the current cars that are being auctioned")
+	result, err = contract.EvaluateTransaction("QueryAllCars")
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %v", err)
+	}
+	log.Println(string(result))
+
+	/****************************   Bids   ***********************************/
+	log.Println("--> Evaluate Transaction: Querybid, returns the bid stored in the world state with given id")
+	result, err = contract.EvaluateTransaction("Querybid", "2")
+	if err != nil {
+		log.Fatalf("Failed to Evaluate transaction: %v", err)
+	}
+	log.Println(string(result))
+
+	log.Println("--> Evaluate Transaction: QueryAllBidsForCarNumber, returns all bids made for the car with a given id found in world state")
+	result, err = contract.EvaluateTransaction("QueryAllCars", "6")
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %v", err)
+	}
+	log.Println(string(result))
+	
+	/****************************   Auctioner Methods   ***********************************/
+	log.Println("--> Submit Transaction: CreateCar, function create an auction process for a car with details provided")
+	result, err = contract.SubmitTransaction("CreateCar", "88", "77", "Lada", "Blue", "Rafael")
 	if err != nil {
 		log.Fatalf("Failed to submit transaction: %v\n", err)
 	}
 	log.Println(string(result))
 
-	log.Println("--> Submit Transaction: Start, function start the auction of the car with the given id")
-	result, err = contract.SubmitTransaction("Start", "car18552")
+	log.Println("--> Submit Transaction: StartAuction, function start the auction of the car with the given carNumber")
+	result, err = contract.SubmitTransaction("StartAuction", "88", "Rafael")
 	if err != nil {
 		log.Fatalf("Failed to submit transaction: %v\n", err)
 	}
 	log.Println(string(result))
 
-	log.Println("--> Submit Transaction: Close, function end the auction for the car with the given id")
-	result, err = contract.SubmitTransaction("Close", "car18552")
+	log.Println("--> Submit Transaction: CloseAuction, function end the auction for the car with the given carNumber")
+	result, err = contract.SubmitTransaction("CloseAuction", "88", "Rafael")
 	if err != nil {
 		log.Fatalf("Failed to submit transaction: %v\n", err)
 	}
 	log.Println(string(result))
 
-	log.Println("--> Submit Transaction: Validate, function validate the auction process for the car with the given id")
-	result, err = contract.SubmitTransaction("Validate", "car18552")
+	log.Println("--> Submit Transaction: VerifyAuction, function validate the auction process for the car with the given carNumber")
+	result, err = contract.SubmitTransaction("VerifyAuction", "88", "Rafael")
 	if err != nil {
 		log.Fatalf("Failed to submit transaction: %v\n", err)
 	}
 	log.Println(string(result))
 
-	log.Println("--> Submit Transaction: Bid, user bid for the car with the given id the amount specified")
-	result, err = contract.SubmitTransaction("Bid", "car18552", "15 000")
+	/****************************   Buyer Methods   ***********************************/
+	log.Println("--> Submit Transaction: CreateBit, user bid for the car with the given details the amount specified")
+	result, err = contract.SubmitTransaction("CreateBid", "100", "101", "10 000", "Nadia", "5")
 	if err != nil {
 		log.Fatalf("Failed to submit transaction: %v\n", err)
 	}
